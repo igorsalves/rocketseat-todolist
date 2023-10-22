1f5c63f158507f7e3596d5c6aa0ba9837f06030f
Igor Silva Alves <igor.alves@compasso.com.br>
2023-10-22 14:52:15 -0300

    first commit

---
+++ src/test/java/br/com/igorsalves/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igorsalves.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

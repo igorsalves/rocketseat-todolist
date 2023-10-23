7939fc5d57eaa5bb664a098caad6f125e821029b
Igor Silva Alves <igor.alves@compasso.com.br>
2023-10-22 21:18:02 -0300

    feat: added route for create user

---
+++ src/main/java/br/com/igorsalves/todolist/user/UserModel.java
@@ -0,0 +1,31 @@
+package br.com.igorsalves.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_users")
+public class UserModel {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(unique = true)
+  private String username;
+
+  private String name;
+
+  private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
